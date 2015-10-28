0733bb1a68b9e9e652c4b323f04e1c8e82a2745f
Matthijs Meulenbrug <matthijs@meulenbrug.nl>
2015-10-28 17:09:48 +0100

    Removed comments

--- ASINFixer.py
+++ ASINFixer.py
@@ -3,10 +3,6 @@ from mobi6 import MinimalMobiUpdater
 
 def fix_mobi_asin(book_src, asin):
     def apply_fix(file_src, asin):
-
-            # with lopen(file_src, 'wb') as f:
-            #     db.copy_format_to(book_id, fmt, f, index_is_id=True)
-
             with open(file_src, 'r+b') as stream:
                 mu = MinimalMobiUpdater(stream)
                 mu.update(asin, b'EBOK')
